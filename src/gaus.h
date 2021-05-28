834a33822212b8306173a0c19c265eb87c5a53b3
avdosev <avdosev1@yandex.ru>
2021-05-28 18:18:42 +0300

    add omp version

--- src/gaus.h
+++ src/gaus.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <tuple>
 
-template<typename T>
+template<typename T = double>
 class Matrix2D {
 public:
     std::vector<T> data;
@@ -13,6 +13,10 @@ public:
     Matrix2D(size_t n1, size_t n2) : n1(n1), n2(n2), data(n1*n2, 0) {}
 
     T& get(size_t i, size_t j) {
+        return data[i*n2 + j];
+    }
+
+    const T& get(size_t i, size_t j) const {
         return data[i*n2 + j];
     }
 };
@@ -60,6 +64,61 @@ Matrix2D<T> gauss_solve(Matrix2D<T> matrix) {
 
     // Отделяем от общей матрицы ответы
     Matrix2D<T> answer{n1, 1};
+    for (size_t i = 0; i < n1; i++)
+        answer.get(i, 0) = matrix_clone.get(i, n2-1);
+
+    return answer;
+}
+
+template<typename T>
+Matrix2D<T> gauss_solve_omp(Matrix2D<T> matrix) {
+    size_t n1 = matrix.n1; // строки
+    size_t n2 = matrix.n2; // столбцы
+
+    auto matrix_clone = matrix;
+
+    // Прямой ход (Зануление нижнего левого угла)
+    for (size_t k = 0; k < n1; k++) {
+        auto main_k = matrix.get(k, k);
+#pragma omp parallel for
+        for (size_t i = 0; i < n2; i++) {
+            matrix_clone.get(k, i) /= main_k;
+        }
+#pragma omp parallel for
+        for (size_t i = k + 1; i < n1; i++) {
+            double K = matrix_clone.get(i, k) / matrix_clone.get(k, k);
+            for (size_t j = 0; j < n2; j++) {
+                matrix_clone.get(i, j) -= matrix_clone.get(k, j) * K;
+            }
+        }
+#pragma omp parallel for
+        for (size_t i = 0; i < n1; i++)
+            for (size_t j = 0; j < n2; j++)
+                matrix.get(i, j) = matrix_clone.get(i, j);
+    }
+
+    // Обратный ход (Зануление верхнего правого угла)
+    for (size_t k1 = n1; k1 > 0; k1--) {
+        auto k = k1 - 1;
+#pragma omp parallel for
+        for (size_t i1 = n2 - 1; i1 > 0; i1--) {
+            auto i = i1 - 1;
+            matrix_clone.get(k, i) /= matrix.get(k, k);
+        }
+#pragma omp parallel for
+        for (size_t i1 = k; i1 > 0; i1--) {
+            auto i = i1 - 1;
+            T K = matrix_clone.get(i, k) / matrix_clone.get(k, k);
+            for (size_t j1 = n2; j1 > 0; j1--) {
+                auto j = j1 - 1;
+                matrix_clone.get(i, j) -= matrix_clone.get(k, j) * K;
+            }
+        }
+    }
+
+    // Отделяем от общей матрицы ответы
+    Matrix2D<T> answer{n1, 1};
+#pragma omp parallel for
     for (size_t i = 0; i < n1; i++)
         answer.get(i, 0) = matrix_clone.get(i, n2-1);
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <random>
 #include "gaus.h"
 
-void print(std::ostream &s, Matrix2D<double> matrix) {
+void print(std::ostream &s, const Matrix2D<double>& matrix) {
     s << '[';
     for (size_t i = 0; i < matrix.n1; i++) {
         s << '[' << ' ';
@@ -15,16 +17,52 @@ void print(std::ostream &s, Matrix2D<double> matrix) {
     s << ']' << std::endl;
 }
 
+void print(std::ostream &s, const std::vector<double>& arr) {
+    s << '[' << ' ';
+        for (auto item : arr) {
+            s << item << ' ';
+        }
+    s << ']' << std::endl;
+}
+
+std::pair<Matrix2D<double>, std::vector<double>> generate_matrix(size_t n) {
+    std::mt19937 gen{69};
+    Matrix2D<double> m{n, n+1};
+    std::vector<double> solve(n);
+    std::normal_distribution<> d{-10,10};
+
+    for (auto& item: solve) {
+        item = d(gen);
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        double sum = 0;
+        for (size_t j = 0; j < n; j++) {
+            auto item = d(gen);
+            m.get(i, j) = item;
+            sum += item * solve[j];
+        }
+        m.get(i, n) = sum;
+    }
+
+    return {m, solve};
+}
+
+bool equal(std::vector<double>& arr1, std::vector<double>& arr2, double eps = 0.0001) {
+    if (arr1.size() != arr2.size()) return false;
+    for (size_t i = 0; i < arr1.size(); i++) {
+        if (std::abs(arr1[i] - arr2[i]) > eps) return false;
+    }
+    return true;
+}
+
 void example() {
-    Matrix2D<double> task{3, 4};
-    task.data = std::vector<double>{{
-                                            2, 1, -1, 8,
-                                            -3, -1, 2, -11,
-                                            -2, 1, 2, -3
-                                    }};
+    auto [task, solve] = generate_matrix(3);
+    print(std::cout, solve);
     print(std::cout, task);
-    auto result = gauss_solve(task);
+    auto result = gauss_solve_omp(task);
     print(std::cout, result);
+    std::cout << "equal: " << equal(solve, result.data) << std::endl;
 }
 
 int main() {
